c90d64cf7f57fe404b9d70d28b7f068306690241
FAK <50982737+florianamette@users.noreply.github.com>
2024-08-02 14:35:26 +0200

    add ~/.vnc/xstartup

--- setup.sh
+++ setup.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "Must be run as root"
+   exit 1
+fi
+
+print_separator() {
+    python3 -c 'print("-"*30)'
+}
+
 echo "👋 Update Package "
-python3 -c 'print("-"*30)'
+print_separator
 apt update > /dev/null
 
 echo "🛠️ Install Kali Default tools"
-python3 -c 'print("-"*30)'
+print_separator
 apt install kali-defaults -y > /dev/null
 
 echo "🪟Install Kali xfce"
-python3 -c 'print("-"*30)'
+print_separator
 apt install kali-desktop-xfce -y > /dev/null
 startx > /dev/null
 
 echo "👁️ Install VNC Server"
-python3 -c 'print("-"*30)'
+print_separator
 apt install -y novnc x11vnc > /dev/null
 
+mkdir -p ~/.vnc
+cat <<EOT > ~/.vnc/xstartup
+#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &
+EOT
+
+chmod +x ~/.vnc/xstartup
+
 echo "🎖️ Create VNC service"
-python3 -c 'print("-"*30)'
+print_separator
 cp ./vncserver.service /etc/systemd/system/vncserver.service
 systemctl daemon-reload
 echo "framebuffer_width=1920" >> /boot/config.txt
@@ -26,7 +44,7 @@ echo "hdmi_force_hotplug=1" >> /boot/config.txt
 
 
 echo "🏁 Launch and enable VNC service"
-python3 -c 'print("-"*30)'
+print_separator
 systemctl enable vncserver
 systemctl start vncserver
 
