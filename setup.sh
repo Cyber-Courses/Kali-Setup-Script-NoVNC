bcfb684337c3b72e746091ca2fcd85635c992aa6
FAK <50982737+florianamette@users.noreply.github.com>
2024-08-02 23:17:42 +0200

    seperate in two service and change to tightvncserver

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ print_separator() {
 }
 
 print_separator
-echo "👋 Update Package "
+echo "👋 Update Package List"
 print_separator
 apt update
 
@@ -19,44 +19,44 @@ print_separator
 apt install kali-defaults -y
 
 print_separator
-echo "🪟Install Kali xfce"
+echo "🪟Install xfce"
 print_separator
-apt install kali-desktop-xfce -y
+apt install kali-desktop-xfce xfce4-goodies -y
 
 print_separator
 echo "👁️ Install VNC Server"
 print_separator
-apt install -y novnc x11vnc xvfb
-apt autoremove
+apt install -y novnc tightvncserver websockify
 
 print_separator
-echo "🎖️ Create VNC service"
+echo "🔧 Auto remove unused packages"
+print_separator
+apt autoremove -y
+
+mkdir -p ~/.vnc
+
+cat <<EOL > /home/kali/.vnc/xstartup
+#!/bin/sh
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+startxfce4 &
+EOL
+
+print_separator
+echo "🎖️ Create VNC & NoVNC services"
 print_separator
 
-cat <<EOT > /usr/local/bin/start_x11vnc.sh
-#!/bin/bash
-# Start the XFCE desktop environment
-xvfb-run :1 -screen 0 1920x1080x16 & startxfce4 --display :1 &
-
-# Start x11vnc server
-/usr/bin/x11vnc -display :0 -autoport -localhost -nopw -bg -xkb -ncache -ncache_cr -quiet -forever
-
-# Start novnc
-/usr/share/novnc/utils/novnc_proxy --listen 8081 --vnc localhost:5900
-EOT
-
-chmod 700 /usr/local/bin/start_x11vnc.sh
 
 cp ./vncserver.service /etc/systemd/system/vncserver.service
+cp ./novnc.service /etc/systemd/system/novnc.service
 systemctl daemon-reload
-echo "framebuffer_width=1920" >> /boot/config.txt
-echo "framebuffer_height=1080" >> /boot/config.txt
-echo "hdmi_force_hotplug=1" >> /boot/config.txt
 
 
-echo "🏁 Launch and enable VNC service"
+echo "🏁 Launch and enable VNC & NoVNC services"
 print_separator
 systemctl enable vncserver
 systemctl start vncserver
+systemctl enable novnc
+systemctl start novnc
 
 echo "✅ Everything ready"
