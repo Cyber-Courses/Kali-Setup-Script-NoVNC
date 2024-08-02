30d033da7106ffb7a2f9bb81000a42270b06e857
FAK <50982737+florianamette@users.noreply.github.com>
2024-08-02 14:15:02 +0200

    add startx

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ apt install kali-defaults -y > /dev/null
 echo "🪟Install Kali xfce"
 python3 -c 'print("-"*30)'
 apt install kali-desktop-xfce -y > /dev/null
+startx > /dev/null
 
 echo "👁️ Install VNC Server"
 python3 -c 'print("-"*30)'
@@ -18,6 +19,7 @@ apt install -y novnc x11vnc > /dev/null
 echo "🎖️ Create VNC service"
 python3 -c 'print("-"*30)'
 cp ./vncserver.service /etc/systemd/system/vncserver.service
+systemctl daemon-reload
 echo "framebuffer_width=1920" >> /boot/config.txt
 echo "framebuffer_height=1080" >> /boot/config.txt
 echo "hdmi_force_hotplug=1" >> /boot/config.txt
