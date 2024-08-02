22bec23aab8e158403318a9f2c9afd355de089f9
FAK <50982737+florianamette@users.noreply.github.com>
2024-08-02 15:09:34 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -24,17 +24,24 @@ echo "👁️ Install VNC Server"
 print_separator
 apt install -y novnc x11vnc > /dev/null
 
-mkdir -p ~/.vnc
-cat <<EOT > ~/.vnc/xstartup
-#!/bin/bash
-xrdb $HOME/.Xresources
-startxfce4 &
-EOT
-
-chmod +x ~/.vnc/xstartup
 
 echo "🎖️ Create VNC service"
 print_separator
+
+cat <<EOT > /usr/local/bin/start_x11vnc.sh
+#!/bin/bash
+# Start the XFCE desktop environment
+/usr/bin/startxfce4 &
+
+# Start x11vnc server
+/usr/bin/x11vnc -display :0 -autoport -localhost -nopw -bg -xkb -ncache -ncache_cr -quiet -forever
+
+# Start novnc
+/usr/share/novnc/utils/novnc_proxy --listen 8081 --vnc localhost:5900
+EOT
+
+chmod 700 /usr/local/bin/start_x11vnc.sh
+
 cp ./vncserver.service /etc/systemd/system/vncserver.service
 systemctl daemon-reload
 echo "framebuffer_width=1920" >> /boot/config.txt
